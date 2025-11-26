ccb841bd7c1ac9f06dc6152d61c37e943d0af830
GhiuzanRoxana <roxanaghiuzan18@gmail.com>
2025-11-26 18:30:05 +0200

    lab2

--- src/HCI.AIAssistant.API/Models/DTOs/AiAssistantController/AIAssistantControllerPostMessageRequestDTO.cs
+++ src/HCI.AIAssistant.API/Models/DTOs/AiAssistantController/AIAssistantControllerPostMessageRequestDTO.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace HCI.AIAssistant.API.Models.DTOs.AIAssistantController;
 
 public class AIAssistantControllerPostMessageRequestDTO
